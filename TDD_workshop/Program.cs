9506a7143a91c6a9912c622d814e09dfcfa12f4b
Štrbák Martin (ERNI) <martin.strbak@betterask.erni>
2023-05-10 10:23:22 +0200

    Add project files.

---
+++ TDD_workshop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TDD_workshop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
